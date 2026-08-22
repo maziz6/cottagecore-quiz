7427b8a9a6e9be7308dcd15bf2d0b3be1d44e5b6
Momina Aziz <127369397+maziz6@users.noreply.github.com>
2026-08-21 22:29:28 -0500

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig, Plugin } from "vite";
 import react from "@vitejs/plugin-react";
 import path from "node:path";
-import { createServer } from "./server";
+
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
@@ -25,15 +25,3 @@ export default defineConfig(({ mode }) => ({
   },
 }));
 
-function expressPlugin(): Plugin {
-  return {
-    name: "express-plugin",
-    apply: "serve", // Only apply during development (serve mode)
-    configureServer(server) {
-      const app = createServer();
-
-      // Add Express app as middleware to Vite dev server
-      server.middlewares.use(app);
-    },
-  };
-}
